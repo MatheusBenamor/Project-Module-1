c18797e3bff35d52bba894cddfbf17cd7d34c85c
Matheus Benamor <matheusqueirozbenamor@gmail.com>
2021-12-09 22:11:42 +0000

    HTML/CSS-almost-done

--- javascript.js
+++ javascript.js
@@ -1,19 +1,88 @@
 class Game {
 constructor(){
-    this.canvas
-
+    this.canvas = document.createElement('canvas')
+    this.frames = 0;
+    this.obstacles = [];
 }
     start = () => {
+        this.canvas.width = 800;
+        this.canvas.height = 300;
+        this.ctx = this.canvas.getContext('2d')
+        document.getElementById('game-area').appendChild(this.canvas);
+        this.interval = setInterval(updateGameArea, 40);
+    }
 
+    //Vai limpar a tela, para dar a impressão de movimentação
+    clear = () => {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const game = new Game();
+
+class Component {
+    constructor(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.posX = x;
+        this.posY = y;
+        this.speedX = 0;
+        this.speedY = 0;
+                
+    }
+
+    update = () => {
+        const ctx = game.ctx;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.posX, this.posY, this.width, this.height);
+    }
+
+    newPos = () => {
+        this.posX += this.speedX;
+        this.posY += this.speedY;
+    }
+}
+
+const player = new Component(40, 40, "#FFFF66", 10, 210);
+const circle = new Component();
+const triangle = new Component();
+
+//função responsável pelo movimento
+function updateGameArea() {
+    game.clear();
+    player.update();
+}
+
+function createObstacle() {
+    const posX = game.canvas.width;
+    
+}
+
+
+game.start();
+
+window.addEventListener('load', () => {
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+    case 'a':
+     Component.color = 'purple';
+     break;
+   case 's':
+     Component.color = 'red';
+     break;
+   case 'd':
+     Component.color = 'blue';
+     break;
+
+    }
+})
+})
 
 //class Formas{
 //Forma Círculo
-
+// circle = new Component(150, 150, 25, 0, 20 * Math.PI);
+/*
 ctx.beginPath();
 ctx.fillStyle = '#50BFE6'
 ctx.arc(150, 150, 25, 0, 2 * Math.PI);
@@ -41,5 +110,5 @@ ctx.bezierCurveTo(42.5,12.5,37.5,18.5,37.5,18);
 ctx.fill();
 
 
+*/
 
-//}
